8ede8cf6eb3ba35cf2e3f3e04aad53cd858968dc
Marko Dujović <codenamerevy@gmail.com>
2020-07-21 14:04:02 +0200

    1.0.0 Initial commit

---
+++ src/main/java/com/codenamerevy/horizontalpanes/HorizontalPanes.java
@@ -0,0 +1,64 @@
+package com.codenamerevy.horizontalpanes;
+
+import com.codenamerevy.horizontalpanes.content.blocks.ModBlocks;
+import com.codenamerevy.horizontalpanes.content.items.ModItems;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("horizontalpanes")
+public class HorizontalPanes
+{
+    public static final String MODID = "horizontalpanes";
+
+    public HorizontalPanes()
+    {
+        final ModLoadingContext modLoadingContext = ModLoadingContext.get();
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::clientSetup);
+
+        //Registering deferred registers
+        ModBlocks.BLOCKS.register(modEventBus);
+        ModItems.ITEMS.register(modEventBus);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent common)
+    {
+
+    }
+    private void clientSetup(final FMLClientSetupEvent client)
+    {
+
+        //Rendering for blocks occurs on the Client side.
+        RenderTypeLookup.setRenderLayer(ModBlocks.HORIZONTAL_PANE.get(), RenderType.getCutout());
+
+        RenderTypeLookup.setRenderLayer(ModBlocks.GLASS_PANE.get(), RenderType.getCutout());
+
+        RenderTypeLookup.setRenderLayer(ModBlocks.WHITE_STAINED_PANE.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(ModBlocks.RED_STAINED_PANE.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(ModBlocks.ORANGE_STAINED_PANE.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(ModBlocks.PINK_STAINED_PANE.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(ModBlocks.YELLOW_STAINED_PANE.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(ModBlocks.LIME_STAINED_PANE.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(ModBlocks.GREEN_STAINED_PANE.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(ModBlocks.LIGHT_BLUE_STAINED_PANE.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(ModBlocks.CYAN_STAINED_PANE.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(ModBlocks.BLUE_STAINED_PANE.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(ModBlocks.MAGENTA_STAINED_PANE.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(ModBlocks.PURPLE_STAINED_PANE.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(ModBlocks.BROWN_STAINED_PANE.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(ModBlocks.GRAY_STAINED_PANE.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(ModBlocks.LIGHT_GRAY_STAINED_PANE.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(ModBlocks.BLACK_STAINED_PANE.get(), RenderType.getTranslucent());
+    }
+}
---
+++ src/main/java/com/codenamerevy/horizontalpanes/content/blocks/HorizontalPaneBlock.java
@@ -0,0 +1,95 @@
+package com.codenamerevy.horizontalpanes.content.blocks;
+
+import net.minecraft.block.*;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.pathfinding.PathType;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
+import net.minecraftforge.common.ToolType;
+
+@SuppressWarnings("deprecation")
+public class HorizontalPaneBlock extends GlassBlock implements IWaterLoggable
+{
+    //Values for Voxel shape of a block. Doesn't have to be like this, but for readability I've put it like this.
+    private double nodeX = 0.0D;
+    private double nodeY = 6.0D;
+    private double nodeZ = 0.0D;
+
+    private double extX = 16.0D;
+    private double extY = 8.0D;
+    private double extZ = 16.0D;
+
+    private static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+    private final VoxelShape SHAPE = Block.makeCuboidShape(nodeX, nodeY, nodeZ, extX, extY, extZ);
+
+    public HorizontalPaneBlock(Properties properties) {
+        super(properties.notSolid().hardnessAndResistance(0.3F, 0.3F).sound(SoundType.GLASS)); //notSolid for rendering and hardnessAndResistance to match the glass block.
+        this.setDefaultState(this.getStateContainer().getBaseState().with(WATERLOGGED, false));
+    }
+
+    //Returns which side of the block will be invisible and when.
+    public boolean isSideInvisible(BlockState state, BlockState adjacentBlockState, Direction side) {
+        return adjacentBlockState.isIn(this) ? true : super.isSideInvisible(state, adjacentBlockState, side);
+    }
+
+    //Returns the value of light that can pass through the block???
+    public float getAmbientOcclusionLightValue(BlockState state, IBlockReader worldIn, BlockPos pos) {
+        return 1.0F;
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder)
+    {
+        builder.add(WATERLOGGED);
+    }
+
+    //Returns the type of a fluid that's waterlogging the block.
+    @Override
+    public FluidState getFluidState(BlockState state)
+    {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
+    }
+
+    //This is used to update the block state. In this situation updates the block if it's placed in water, or water is placed 'in' the block.
+    @Override
+    public BlockState updatePostPlacement(BlockState state, Direction facing, BlockState facingState, IWorld world, BlockPos currentPos, BlockPos facingPos)
+    {
+        if (state.get(WATERLOGGED))
+        {
+            world.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+        return super.updatePostPlacement(state, facing, facingState, world, currentPos, facingPos);
+    }
+
+    //This forms a block shape from the given values.
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext context)
+    {
+        return SHAPE;
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context)
+    {
+        FluidState fluidState = context.getWorld().getFluidState(context.getPos());
+        return this.getDefaultState().with(WATERLOGGED, Boolean.valueOf(fluidState.isTagged(FluidTags.WATER) && fluidState.getLevel() == 8));
+    }
+
+
+    //This allows the block to be moved by pistons.
+    @Override
+    public boolean allowsMovement(BlockState state, IBlockReader world, BlockPos pos, PathType path)
+    {
+        return true;
+    }
+}
