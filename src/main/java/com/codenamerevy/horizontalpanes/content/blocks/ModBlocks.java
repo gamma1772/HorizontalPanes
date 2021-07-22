8d03232252cb09c12311a2b70e2b2280add6e01f
Marko Dujović <dujovicm@outlook.com>
2021-07-22 17:18:55 +0200

    Refactor Block initialization

--- src/main/java/com/codenamerevy/horizontalpanes/content/blocks/ModBlocks.java
+++ src/main/java/com/codenamerevy/horizontalpanes/content/blocks/ModBlocks.java
@@ -3,8 +3,13 @@ package com.codenamerevy.horizontalpanes.content.blocks;
 import com.codenamerevy.horizontalpanes.HorizontalPanes;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -16,23 +21,37 @@ public class ModBlocks
 
     //public static final RegistryObject<Block> HORIZONTAL_PANE           = BLOCKS.register("horizontal_pane",                    () -> new HorizontalPaneSlab(AbstractBlock.Properties.create(Material.GLASS)));
 
-    public static final RegistryObject<Block> GLASS_PANE                = BLOCKS.register("horizontal_glass_pane",              () -> new HorizontalPaneSlab(AbstractBlock.Properties.create(Material.GLASS)));
+    public static final RegistryObject<Block> GLASS_PANE                = BLOCKS.register("horizontal_glass_pane", ModBlocks::createHorizontalPane);
 
-    public static final RegistryObject<Block> WHITE_STAINED_PANE        = BLOCKS.register("horizontal_stained_white_pane",      () -> new HorizontalPaneSlab(AbstractBlock.Properties.create(Material.GLASS, MaterialColor.SNOW)));
-    public static final RegistryObject<Block> RED_STAINED_PANE          = BLOCKS.register("horizontal_stained_red_pane",        () -> new HorizontalPaneSlab(AbstractBlock.Properties.create(Material.GLASS, MaterialColor.RED)));
-    public static final RegistryObject<Block> ORANGE_STAINED_PANE       = BLOCKS.register("horizontal_stained_orange_pane",     () -> new HorizontalPaneSlab(AbstractBlock.Properties.create(Material.GLASS, MaterialColor.ADOBE)));
-    public static final RegistryObject<Block> PINK_STAINED_PANE         = BLOCKS.register("horizontal_stained_pink_pane",       () -> new HorizontalPaneSlab(AbstractBlock.Properties.create(Material.GLASS, MaterialColor.PINK)));
-    public static final RegistryObject<Block> YELLOW_STAINED_PANE       = BLOCKS.register("horizontal_stained_yellow_pane",     () -> new HorizontalPaneSlab(AbstractBlock.Properties.create(Material.GLASS, MaterialColor.YELLOW)));
-    public static final RegistryObject<Block> LIME_STAINED_PANE         = BLOCKS.register("horizontal_stained_lime_pane",       () -> new HorizontalPaneSlab(AbstractBlock.Properties.create(Material.GLASS, MaterialColor.LIME)));
-    public static final RegistryObject<Block> GREEN_STAINED_PANE        = BLOCKS.register("horizontal_stained_green_pane",      () -> new HorizontalPaneSlab(AbstractBlock.Properties.create(Material.GLASS, MaterialColor.GREEN)));
-    public static final RegistryObject<Block> LIGHT_BLUE_STAINED_PANE   = BLOCKS.register("horizontal_stained_light_blue_pane", () -> new HorizontalPaneSlab(AbstractBlock.Properties.create(Material.GLASS, MaterialColor.LIGHT_BLUE)));
-    public static final RegistryObject<Block> CYAN_STAINED_PANE         = BLOCKS.register("horizontal_stained_cyan_pane",       () -> new HorizontalPaneSlab(AbstractBlock.Properties.create(Material.GLASS, MaterialColor.CYAN)));
-    public static final RegistryObject<Block> BLUE_STAINED_PANE         = BLOCKS.register("horizontal_stained_blue_pane",       () -> new HorizontalPaneSlab(AbstractBlock.Properties.create(Material.GLASS, MaterialColor.BLUE)));
-    public static final RegistryObject<Block> MAGENTA_STAINED_PANE      = BLOCKS.register("horizontal_stained_magenta_pane",    () -> new HorizontalPaneSlab(AbstractBlock.Properties.create(Material.GLASS, MaterialColor.MAGENTA)));
-    public static final RegistryObject<Block> PURPLE_STAINED_PANE       = BLOCKS.register("horizontal_stained_purple_pane",     () -> new HorizontalPaneSlab(AbstractBlock.Properties.create(Material.GLASS, MaterialColor.PURPLE)));
-    public static final RegistryObject<Block> BROWN_STAINED_PANE        = BLOCKS.register("horizontal_stained_brown_pane",      () -> new HorizontalPaneSlab(AbstractBlock.Properties.create(Material.GLASS, MaterialColor.BROWN)));
-    public static final RegistryObject<Block> GRAY_STAINED_PANE         = BLOCKS.register("horizontal_stained_gray_pane",       () -> new HorizontalPaneSlab(AbstractBlock.Properties.create(Material.GLASS, MaterialColor.GRAY)));
-    public static final RegistryObject<Block> LIGHT_GRAY_STAINED_PANE   = BLOCKS.register("horizontal_stained_light_gray_pane", () -> new HorizontalPaneSlab(AbstractBlock.Properties.create(Material.GLASS, MaterialColor.LIGHT_GRAY)));
-    public static final RegistryObject<Block> BLACK_STAINED_PANE        = BLOCKS.register("horizontal_stained_black_pane",      () -> new HorizontalPaneSlab(AbstractBlock.Properties.create(Material.GLASS, MaterialColor.BLACK)));
+    public static final RegistryObject<Block> WHITE_STAINED_PANE        = BLOCKS.register("horizontal_stained_white_pane",      () -> createHorizontalPane(MaterialColor.SNOW));
+    public static final RegistryObject<Block> RED_STAINED_PANE          = BLOCKS.register("horizontal_stained_red_pane",        () -> createHorizontalPane(MaterialColor.RED));
+    public static final RegistryObject<Block> ORANGE_STAINED_PANE       = BLOCKS.register("horizontal_stained_orange_pane",     () -> createHorizontalPane(MaterialColor.ADOBE));
+    public static final RegistryObject<Block> PINK_STAINED_PANE         = BLOCKS.register("horizontal_stained_pink_pane",       () -> createHorizontalPane(MaterialColor.PINK));
+    public static final RegistryObject<Block> YELLOW_STAINED_PANE       = BLOCKS.register("horizontal_stained_yellow_pane",     () -> createHorizontalPane(MaterialColor.YELLOW));
+    public static final RegistryObject<Block> LIME_STAINED_PANE         = BLOCKS.register("horizontal_stained_lime_pane",       () -> createHorizontalPane(MaterialColor.LIME));
+    public static final RegistryObject<Block> GREEN_STAINED_PANE        = BLOCKS.register("horizontal_stained_green_pane",      () -> createHorizontalPane(MaterialColor.GREEN));
+    public static final RegistryObject<Block> LIGHT_BLUE_STAINED_PANE   = BLOCKS.register("horizontal_stained_light_blue_pane", () -> createHorizontalPane(MaterialColor.LIGHT_BLUE));
+    public static final RegistryObject<Block> CYAN_STAINED_PANE         = BLOCKS.register("horizontal_stained_cyan_pane",       () -> createHorizontalPane(MaterialColor.CYAN));
+    public static final RegistryObject<Block> BLUE_STAINED_PANE         = BLOCKS.register("horizontal_stained_blue_pane",       () -> createHorizontalPane(MaterialColor.BLUE));
+    public static final RegistryObject<Block> MAGENTA_STAINED_PANE      = BLOCKS.register("horizontal_stained_magenta_pane",    () -> createHorizontalPane(MaterialColor.MAGENTA));
+    public static final RegistryObject<Block> PURPLE_STAINED_PANE       = BLOCKS.register("horizontal_stained_purple_pane",     () -> createHorizontalPane(MaterialColor.PURPLE));
+    public static final RegistryObject<Block> BROWN_STAINED_PANE        = BLOCKS.register("horizontal_stained_brown_pane",      () -> createHorizontalPane(MaterialColor.BROWN));
+    public static final RegistryObject<Block> GRAY_STAINED_PANE         = BLOCKS.register("horizontal_stained_gray_pane",       () -> createHorizontalPane(MaterialColor.GRAY));
+    public static final RegistryObject<Block> LIGHT_GRAY_STAINED_PANE   = BLOCKS.register("horizontal_stained_light_gray_pane", () -> createHorizontalPane(MaterialColor.LIGHT_GRAY));
+    public static final RegistryObject<Block> BLACK_STAINED_PANE        = BLOCKS.register("horizontal_stained_black_pane",      () -> createHorizontalPane(MaterialColor.BLACK));
 
+    private static HorizontalPaneSlab createHorizontalPane() {
+        return new HorizontalPaneSlab(AbstractBlock.Properties.create(Material.GLASS));
+    }
+
+    private static HorizontalPaneSlab createHorizontalPane(MaterialColor color) {
+        return new HorizontalPaneSlab(AbstractBlock.Properties.create(Material.GLASS, color));
+    }
+
+    private static HorizontalPaneSlab createHorizontalPane(Material material, MaterialColor color, SoundType sound) {
+        return new HorizontalPaneSlab(AbstractBlock.Properties.create(material, color).sound(sound));
+    }
+
+    private static Boolean never(BlockState blockState, IBlockReader blockView, BlockPos blockPos, EntityType<?> entityType) { return false; }
+    private static boolean never(BlockState blockState, IBlockReader blockView, BlockPos blockPos) { return false; }
 }
