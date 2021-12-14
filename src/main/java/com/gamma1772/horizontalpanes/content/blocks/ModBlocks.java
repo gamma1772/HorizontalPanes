9c4a5f87bd2aab376f9e83f09d714f1642945bb0
Conner Blair <cblair1353@gmail.com>
2021-12-13 22:25:56 -0800

    1.18.1 update; untested

--- src/main/java/com/gamma1772/horizontalpanes/content/blocks/ModBlocks.java
+++ src/main/java/com/gamma1772/horizontalpanes/content/blocks/ModBlocks.java
@@ -33,9 +33,9 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlocks
 {
